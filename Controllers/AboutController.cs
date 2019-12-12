c3ba2ff93d614fc061aa88ddfe32259a003a8f36
wujialiang999 <1647113057@qq.com>
2019-12-12 20:17:35 +0800

    属性路由

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -1,8 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace dotnetcore3stu{
+    // [Route("about")]
+    // [Route("[controller]")] //使用Controller的名字
+    [Route("[controller]/[action]")]
+    // [Route("v2/[controller]/[action]")] //http://loalhost:5000/v2/about/me
     public class AboutController{
+        // [Route("me")]
+        // [Route("[action]")]
         public string Me(){
             return "wjl";
         }
+        // [Route("")]//默认返回
         public string Company(){
             return "没公司";
         }
