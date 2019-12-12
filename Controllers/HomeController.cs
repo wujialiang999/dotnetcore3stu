fce5974287fb3141a55314ced279d36e09c69cb3
wujialiang999 <1647113057@qq.com>
2019-12-12 20:44:04 +0800

    返回IActionResult

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 namespace dotnetcore3stu
 {
-    public class HomeController{
-        public string Index(){
-            return "Hello from HomeController";
+    public class HomeController :Controller{
+        public IActionResult Index(){
+            // this.HttpContext.Request;
+            // return this.Ok(1);
+            // return this.File();
+            return this.Content("文字内容");
+            // return "Hello from HomeController";
         }
     }
 }
